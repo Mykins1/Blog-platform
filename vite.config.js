4c8b8d903ae8dc5fd4a77234b21d80708596efb8
Mykins1 <akinsademykel@gmail.com>
2025-08-05 00:09:35 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/socia-blog",
+  base: "/Socia-blog",
   plugins: [react(), tailwindcss()],
 });
