588b4d683c2c8ba5d9874bf955fbec901b323ece
Mykins1 <akinsademykel@gmail.com>
2025-08-05 10:14:31 +0100

    added help

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Socia-blog/",
+  base: "/Socia-blog",
   plugins: [react(), tailwindcss(
     {
       theme: {
