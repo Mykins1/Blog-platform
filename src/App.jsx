ff82fc202002669def054d8e1d5ef19db1b0b7dc
Mykins1 <akinsademykel@gmail.com>
2025-08-07 17:33:31 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import {
   Route,
   useLocation,
 } from "react-router-dom";
-import { NavbarProvider } from "./contexts/Visibility";
 import Navbar, { BottomNav } from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -45,9 +44,7 @@ function AppContent() {
 function App() {
   return (
     <Router>
-      <NavbarProvider>
-        <AppContent />
-      </NavbarProvider>
+      <AppContent />
     </Router>
   );
 }
