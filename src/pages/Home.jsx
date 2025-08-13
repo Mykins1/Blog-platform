434e68c7fed0a4457ab321b079b9f2d792c3e1dd
Mykins1 <akinsademykel@gmail.com>
2025-08-13 11:48:06 +0100

    still fixing everytime reload issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,25 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+import {React} from "react";
 import BlogCard from "../components/BlogCard.jsx";
 import blogData from "../../data/db.js";
 
 export default function Blog() {
-  const [isPending, setIsPending] = useState(true);
-  const error = false;
+  // const [isPending, setIsPending] = useState(true);
+  // const error = false;
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsPending(false);
-    }, 0);
-    return () => clearTimeout(timer);
-  }, []);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setIsPending(false);
+  //   }, 0);
+  //   return () => clearTimeout(timer);
+  // }, []);
 
   return (
     <div
       className=""
       // style={{ fontFamily: "Inter, sans-serif", background: "#fff" }}
     >
-      {isPending ? (
+      {/* {isPending ? (
         <div className="flex items-center justify-center h-150">
           <div className="loading ">Loading...</div>
         </div>
@@ -26,11 +27,11 @@ export default function Blog() {
         <div className="flex items-center justify-center h-150">
           <div className="error">{error}</div>
         </div>
-      ) : (
+      ) : ( */}
         <div className="flex flex-col items-center max-w-4xl mx-auto w-full bg-white md:flex-col">
           {blogData && <BlogCard blogs={blogData.blogs} title="Blogs" />}
         </div>
-      )}
+      {/* )} */}
     </div>
   );
 }
