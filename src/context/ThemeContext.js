6a7f013d2cd52b9494743bc63882ca4543550f41
Mykel Akinsade <akinsademykel@gmail.com>
2025-09-12 11:28:54 +0100

    Update ThemeContext.js
    changed background color

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -19,7 +19,7 @@ export const ThemeProvider = ({ children }) => {
   const themeClasses =
     theme === "dark"
       ? {
-          background: " bg-slate-700",
+          background: " bg-sky-950",
           text: "text-white",
           icon: "text-gray-500",
           hover: "hover:bg-zinc-700/50",
